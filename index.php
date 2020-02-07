9dd6d1801183965dddeb19b198d3e75b5d9896d5
IvanMi773 <imihajlovskij23@gmail.com>
2020-02-07 17:28:50 +0200

    create logic of add data into data.xml

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    <?php require('form.php') ?>
+
+<?php
+
+require ('form.php');
+require ('table.php');
+
+?>
+
 </body>
 </html>
---
+++ table.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<table>
+    <tr>
+        <th>Ім'я</th>
+        <th>Прізвище</th>
+        <th>Email</th>
+        <th>Номер телефону</th>
+        <th>Фото</th>
+    </tr>
+
+</table>
