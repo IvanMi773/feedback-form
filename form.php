c0b3c112234e634df8b141b5bc1a5c2ed3267bd5
IvanMi773 <imihajlovskij23@gmail.com>
2020-02-07 14:39:07 +0200

    initial commit

---
+++ form.php
@@ -0,0 +1,9 @@
+<form action="" enctype="multipart/form-data">
+    <input type="text" name="first_name" placeholder="Введіть ім'я"><br>
+    <input type="text" name="last_name" placeholder="Введіть прізвище"><br>
+    <input type="text" name="email" placeholder="Введіть email"><br>
+    <input type="text" name="phone_number" placeholder="Введіть номер телефону"><br>
+    <input type="file" name="photo"> <br>
+
+    <button type="submit">Надіслати відгук</button>
+</form>
